8ba06f129f4f92e6e5288c4e229491e434e79646
kaushalmeena <kaushal.meena@aasaanjobs.com>
2020-04-26 22:34:01 +0530

    fixed dotenv and breadcrumb issue

--- textsuite/config.py
+++ textsuite/config.py
@@ -2,14 +2,17 @@
 """Python script for TextSuite configuration."""
 
 from dotenv import load_dotenv
-from os import environ
+from os import getenv
+from os.path import join, dirname
 
-load_dotenv(verbose=True)
+dotenv_path = join(dirname(__file__), '.env')
+
+load_dotenv(dotenv_path=dotenv_path, verbose=True)
 
 
 class Config():
-    OCR_API_KEY = environ.get('OCR_API_KEY')
-    WOLFRAMALPHA_APP_ID = environ.get('WOLFRAMALPHA_APP_ID')
+    OCR_API_KEY = getenv('OCR_API_KEY')
+    WOLFRAMALPHA_APP_ID = getenv('WOLFRAMALPHA_APP_ID')
 
 
 class ProductionConfig(Config):
